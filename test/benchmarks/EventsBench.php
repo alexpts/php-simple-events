9013016b84268bcacda85589d95bdb26ea5371d6
alexpts <alexpts@yandex.ru>
2020-12-17 21:14:11 +0300

    update dependencies, try `phpbench`

---
+++ test/benchmarks/EventsBench.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+namespace test\unit;
+
+use PhpBench\Benchmark\Metadata\Annotations\Iterations;
+use PhpBench\Benchmark\Metadata\Annotations\OutputMode;
+use PhpBench\Benchmark\Metadata\Annotations\Revs;
+use PhpBench\Benchmark\Metadata\Annotations\Subject;
+use PhpBench\Benchmark\Metadata\Annotations\Warmup;
+use PTS\Events\EventEmitter;
+
+class EventsBench
+{
+    protected EventEmitter $emitter;
+
+    public function init()
+    {
+        $this->emitter = new EventEmitter;
+        $this->emitter->on('some.event', static fn() => 1);
+    }
+    
+    /**
+     * @Subject bench
+     * @Revs(1000000)
+     * @Iterations(10)
+     * @BeforeMethods({"init"})
+     * @OutputMode("throughput")
+     * @Warmup(1)
+     */
+    public function emit(): void
+    {
+        $this->emitter->emit('some.event');
+    }
+}
